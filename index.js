23b0aa9c727f405deaf1295cbe357cdd3d122654
Kevin Tukgali <kevin.s.t.2006@gmail.com>
2026-07-25 06:58:49 +1000

    fixx

--- index.js
+++ index.js
@@ -278,9 +278,9 @@ function recommendSlots(durationMin, fromDate, priority = 2, excludeId = null, d
 // ---- event endpoints ----
 
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(ROOT, 'index.html'));
-// });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(ROOT, 'index.html'));
+});
 
 
 // browser loads all events on startup
